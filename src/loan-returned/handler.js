020f034df3c8739c28ddae2c10f6fb37ba32390d
Dom Belcher <dominic.belcher@gmail.com>
2018-06-14 14:54:31 +0100

    removed some more redundant code

--- src/loan-returned/handler.js
+++ src/loan-returned/handler.js
@@ -2,18 +2,14 @@ const extractMessageData = require('../extract-message-data')
 const almaCache = require('../cache-from-env')
 
 module.exports.handle = (event, context, callback) => {
-  let loanData
-
   try {
-    loanData = extractMessageData(event)
+    const loanData = extractMessageData(event)
+    almaCache.handleLoanReturned(loanData.item_loan)
+      .then(() => callback(null, generateSuccessMessage(loanData.item_loan.loan_id)))
+      .catch(callback)
   } catch (e) {
     callback(e)
-    return
   }
-
-  almaCache.handleLoanReturned(loanData.item_loan)
-    .then(() => callback(null, generateSuccessMessage(loanData.item_loan.loan_id)))
-    .catch(callback)
 }
 
 const generateSuccessMessage = (id) => {
--- src/loan-updated/handler.js
+++ src/loan-updated/handler.js
@@ -2,18 +2,14 @@ const extractMessageData = require('../extract-message-data')
 const almaCache = require('../cache-from-env')
 
 module.exports.handle = (event, context, callback) => {
-  let loanData
-
   try {
-    loanData = extractMessageData(event)
+    const loanData = extractMessageData(event)
+    almaCache.handleLoanUpdate(loanData.item_loan)
+      .then(() => callback(null, generateSuccessMessage(loanData.item_loan.loan_id)))
+      .catch(callback)
   } catch (e) {
     callback(e)
-    return
   }
-
-  almaCache.handleLoanUpdate(loanData.item_loan)
-    .then(() => callback(null, generateSuccessMessage(loanData.item_loan.loan_id)))
-    .catch(callback)
 }
 
 const generateSuccessMessage = (id) => {
