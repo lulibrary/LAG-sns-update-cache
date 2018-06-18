eae1a8eb1d3ba96d59e5b849e703937c0cee0b50
Dom Belcher <dominic.belcher@gmail.com>
2018-06-18 09:47:46 +0100

    added queue URL environment variable in serverless.yml, updated to Lag-utils#v0.3.3, lag-alma-utils#v.4.0

--- src/cache.js
+++ src/cache.js
@@ -15,7 +15,7 @@ const modelNamesByType = new Map([
 
 class Cache {
   constructor (tables) {
-    this.usersQueue = new Queue(process.env.UsersQueueName, process.env.UsersQueueOwner)
+    this.usersQueue = new Queue({ name: process.env.UsersQueueName, owner: process.env.UsersQueueOwner })
     this.models = {}
     Object.keys(tables).forEach((type) => this.createModelByType(type, tables[type]))
   }
